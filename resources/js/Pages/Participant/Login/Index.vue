8bda4322d18740de41bbd0e28409b48c9ac293cb
ekonurwahyudi <generasi@gmail.com>
2025-06-24 07:40:18 +0700

    update

--- resources/js/Pages/Participant/Login/Index.vue
+++ resources/js/Pages/Participant/Login/Index.vue
@@ -7,7 +7,7 @@
             <div class="bg-white shadow border-0 rounded border-light p-4 p-lg-5 w-100 fmxw-500">
                 <div class="text-center text-md-center mt-md-0">
                 <img src="/assets/images/favicon.png" alt="Brevetisasi DEFA Logo" class="brand-logo mb-2" style="height: 60px;">
-                <h4 class="mt-2" style="margin:0px">Hello, Welcome Back!</h4>
+                <h4 class="mt-2" style="margin:0px">Hello, Welcome Back 👋!</h4>
                 <span class="text-muted">Let’s Empower Your Skills!</span>
             </div>
                 <div v-if="errors.message" class="alert alert-danger mt-2">
